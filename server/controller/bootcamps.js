868cb80c1274df2093378526e7ce77d2cab61c84
Michael W <mwerbo2@gmail.com>
2018-12-27 23:17:32 -0600

    Create bootcamp get route

--- server/controller/bootcamps.js
+++ server/controller/bootcamps.js
@@ -5,28 +5,9 @@ module.exports = {
   create(req, res) {
     return Comment
       .create({
-        name: req.body.name,
-        website: req.body.website,
-        levels: req.body.levels,
-        format: req.body.format,
-        format_description: req.body.format_description,
-        languages: req.body.languages,
-        money_needed: req.body.money_needed,
-        online_only: req.body.online_only,
-        number_of_students: req.body.number_of_students,
-        contact_name: req.body.contact_name,
-        contact_number: req.body.contact_number,
-        contact_email: req.body.contact_email,
-        latitude: req.body.latitude,
-        longitude: req.body.longitude,
-        street: req.body.street,
-        city: req.body.city,
-        state: req.body.state,
-        zip: req.body.zip,
-        published: req.body.published,
-        updated_at: req.body.updated_at,
-        country: req.body.country,
-        source: req.body.source
+        bootcamp_id: req.body.bootcamp_id,
+        user_id: req.body.user_id,
+        comment: req.body.comment,
       })
       .then(comment => res.status(201).send(comment))
       .catch(error => res.status(400).send(error));
