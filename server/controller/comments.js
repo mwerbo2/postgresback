e5acf620a79bbc283b6fe090a8cc62593769ad78
Michael W <mwerbo2@gmail.com>
2018-12-29 17:40:00 -0600

    Add seperate comment route and controller

---
+++ server/controller/comments.js
@@ -0,0 +1,33 @@
+const Comment = require('../models').comments;
+
+module.exports = {
+    create(req, res) {
+        return Comment
+          .create({
+            bootcamp_id: req.body.bootcamp_id,
+            user_id: req.body.user_id,
+            comment: req.body.comment,
+          })
+          .then(comment => res.status(201).send(comment))
+          .catch(error => res.status(400).send(error));
+      },
+      //Return all comments
+      allComments(req, res){
+        return Comment
+        .findAll({})
+        .then(comment => res.status(201).send(comment))
+        .catch(error => res.status(400).send(error));
+      },
+      //Return bootcamp specific comments
+      bootcampComment(req, res){
+        console.log(req.params.id);
+        return Comment
+        .findAll({
+          where: {
+            bootcamp_id: req.params.id
+          }
+        })
+        .then(comment => res.status(201).send(comment))
+        .catch(error => res.status(400).send(error));
+      }
+};
---
+++ server/routes/comments.js
@@ -0,0 +1,10 @@
+module.exports = (app) => {
+    //Return all comments
+    app.get('/comment', bootcampController.allComments);
+
+    //Return comments for a specific bootcamp
+    app.get('/comment/:id', bootcampController.bootcampComment);
+
+    //Create a new comment for a bootcamp
+    app.post('/comment/post', bootcampController.create);
+}
