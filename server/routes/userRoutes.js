4e3039b86ff9f4d3a2669ad2eade03ed9552b0f1
Michael Werbowsky <mwerbo2@gmail.com>
2019-01-07 23:32:21 -0600

    Add in auth

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,4 +6,6 @@ module.exports = (app) => {
     app.get('/user/:email', userController.findUser);
 
     app.post('/user/new', userController.create);
+
+    //Set up authentication routes for post, update, and delete
 }
